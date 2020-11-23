23bef9b1ff8763b0048f76fc607a355b62368195
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2020-11-23 17:16:59 +0100

    Rewrite components

--- src/screens/HomePage/HomePage.jsx
+++ src/screens/HomePage/HomePage.jsx
@@ -25,68 +25,55 @@ import myImage from '../../assets/visuels/bandeauEvenement2.jpg'
 
 import './HomePage.scss'
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class HomePage extends React.PureComponent {
-  render() {
-    const { history, user } = this.props
-
-    return (
-      <div className="home-page-div">
-        <p className="home-speech">
-          Bienvenue sur Road to russia 2018, le site de pronostics de la coupe du monde. Jouez en
-          famille ou entre amis sur les scores du mondial et affrontez d&apos;autres tribus ! Le but
-          ? Pariez au plus proche de la réalité les résultats des équipes, marquez des points, et
-          tentez de gagner la première place.
+const HomePage = ({ history, user }) => {
+  return (
+    <div className="home-page-div">
+      <p className="home-speech">
+        Bienvenue sur Road to russia 2018, le site de pronostics de la coupe du monde. Jouez en
+        famille ou entre amis sur les scores du mondial et affrontez d&apos;autres tribus ! Le but
+        ? Pariez au plus proche de la réalité les résultats des équipes, marquez des points, et
+        tentez de gagner la première place.
           <br />
-          <br />
+        <br />
           Fans de foot aux grands cœurs, le site s&apos;engage à reverser 50% des gains à
           l&apos;association humanitaire{' '}
-          <a title="Site PAM" href="https://pourunailleursmeilleur.wordpress.com/" target="_blank" rel="noreferrer">
-            PAM - Pour un Ailleurs Meilleur
+        <a title="Site PAM" href="https://pourunailleursmeilleur.wordpress.com/" target="_blank" rel="noreferrer">
+          PAM - Pour un Ailleurs Meilleur
           </a>.
         </p>
 
-        <div className="home-buttons-div">
-          <div className="home-button-panel">
-            <p>Les règles du jeu :</p>
-            <Button className="home-button" onClick={() => history.push('/rules')} color="primary">
-              <ListIcon className="icon-left" />
+      <div className="home-buttons-div">
+        <div className="home-button-panel">
+          <p>Les règles du jeu :</p>
+          <Button className="home-button" onClick={() => history.push('/rules')} color="primary">
+            <ListIcon className="icon-left" />
               Règles
             </Button>
-          </div>
-          {!isEmpty(user) && (
-            <Fragment>
-              <div className="home-button-panel">
-                <p>Tous vos paris : </p>
-                <Button
-                  className="home-button"
-                  onClick={() => history.push('/matches')}
-                  color="primary"
-                >
-                  <EventAvailableIcon className="icon-left" />
+        </div>
+        {!isEmpty(user) && <Fragment>
+          <div className="home-button-panel">
+            <p>Tous vos paris : </p>
+            <Button className="home-button" onClick={() => history.push('/matches')} color="primary">
+              <EventAvailableIcon className="icon-left" />
                   Parier
                 </Button>
-              </div>
-              <div className="home-button-panel">
-                <p>Votre classement : </p>
-                <Button
-                  className="home-button"
-                  onClick={() => history.push('/ranking')}
-                  color="primary"
-                >
-                  <PollIcon className="icon-left" />
+          </div>
+          <div className="home-button-panel">
+            <p>Votre classement : </p>
+            <Button className="home-button" onClick={() => history.push('/ranking')} color="primary">
+              <PollIcon className="icon-left" />
                   Classement
                 </Button>
-              </div>
-            </Fragment>
-          )}
-        </div>
-        {!isEmpty(user) && <Winner />}
-        <img alt="Home" className="home-logo" src={myImage} />
+          </div>
+        </Fragment>}
       </div>
-    )
-  }
+      {!isEmpty(user) && <Winner />}
+      <img alt="Home" className="home-logo" src={myImage} />
+    </div>
+  )
 }
+
+export default HomePage
 
 HomePage.propTypes = {
   history: PropTypes.shape({
--- src/screens/Ranking/Ranking.jsx
+++ src/screens/Ranking/Ranking.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import isEmpty from 'lodash/isEmpty'
@@ -10,51 +10,42 @@ import GroupRanking from './GroupRanking'
 
 import './ranking.scss'
 
-class Ranking extends Component {
-  state = { selectedTab: 0 }
+const Ranking = ({ groups = [] }) => {
+  const [selectedTab, setselectedTab] = useState()
 
-  handleTabChange = (event, value) => {
-    this.setState({ selectedTab: value })
+  const handleTabChange = (event, value) => {
+    setselectedTab(value)
   }
 
-  render() {
-    const { groups } = this.props
-    const { selectedTab } = this.state
-
-    return isEmpty(groups) ? (
+  return isEmpty(groups) ? (
+    <Fragment>
+      <div className="ranking-page-div">
+        <p>
+          Pour pouvoir vous visualiser dans le classement, il vous faut tout d&apos;abord{' '}
+          <Link to="/groups">créer ou rejoindre une tribu</Link>
+        . Si cela est dejà fait, vérifier que vous avez bien payé le droit d&apos;entrée sur la{' '}
+          <a
+            title="Site cagnotte"
+            href="https://www.paypal.com/pools/c/84gsKV8QG8"
+            target="_blank" rel="noreferrer"
+          >
+            cagnotte
+        </a>. Si cela est également fait, veuillez attendre la validation des administrateurs.
+      </p>
+      </div>
+    </Fragment>
+  ) : (
       <Fragment>
-        <div className="ranking-page-div">
-          <p>
-            Pour pouvoir vous visualiser dans le classement, il vous faut tout d&apos;abord{' '}
-            <Link to="/groups">créer ou rejoindre une tribu</Link>
-            . Si cela est dejà fait, vérifier que vous avez bien payé le droit d&apos;entrée sur la{' '}
-            <a
-              title="Site cagnotte"
-              href="https://www.paypal.com/pools/c/84gsKV8QG8"
-              target="_blank" rel="noreferrer"
-            >
-              cagnotte
-            </a>. Si cela est également fait, veuillez attendre la validation des administrateurs.
-          </p>
+        <AppBar position="fixed" className="ranking-tab-bar">
+          <Tabs value={selectedTab} onChange={handleTabChange} centered>
+            {groups.map(group => <Tab key={group.id} label={group.name} />)}
+          </Tabs>
+        </AppBar>
+        <div className="ranking-container">
+          {!isEmpty(groups) && <GroupRanking {...groups[selectedTab]} />}
         </div>
       </Fragment>
-    ) : (
-        <Fragment>
-          <AppBar position="fixed" className="ranking-tab-bar">
-            <Tabs value={selectedTab} onChange={this.handleTabChange} centered>
-              {groups.map(group => <Tab key={group.id} label={group.name} />)}
-            </Tabs>
-          </AppBar>
-          <div className="ranking-container">
-            {!isEmpty(groups) && <GroupRanking {...groups[selectedTab]} />}
-          </div>
-        </Fragment>
-      )
-  }
-}
-
-Ranking.defaultProps = {
-  groups: [],
+    )
 }
 
 Ranking.propTypes = {
