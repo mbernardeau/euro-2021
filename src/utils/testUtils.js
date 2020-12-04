3d60db2d6207e766243c4b7dfd9dc53a24b906e8
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2020-12-04 14:22:24 +0100

    Install firebase-tools

--- src/utils/testUtils.js
+++ src/utils/testUtils.js
@@ -1,16 +1,17 @@
 import { initializeTestApp, apps } from '@firebase/rules-unit-testing'
 
+export const TEST_PROJECT_ID = 'euro2021'
 export const TEST_UID = 'test uid'
 
 export const createApp = () =>
   initializeTestApp({
-    projectId: 'euro2021-3d006',
+    projectId: TEST_PROJECT_ID,
     auth: { uid: TEST_UID },
   })
 
 export const createUnauthenticatedApp = () =>
   initializeTestApp({
-    projectId: 'euro2021-3d006',
+    projectId: TEST_PROJECT_ID,
   })
 
 export const ruleTestingCleanup = () => {
