4db1fa8237c3bd161d0628d9f21ac19813646be9
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2020-12-16 09:12:08 +0100

    Use batch in applyInGroup

--- functions/applyInGroup.js
+++ functions/applyInGroup.js
@@ -2,29 +2,33 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const db = admin.firestore()
 
+const isPayingGroup = async (groupRef) => {
+  const group = (await groupRef.get()).data()
+  return group.price > 0
+}
+
 exports.applyInGroup = functions
   .region('europe-west3')
   .firestore.document('groupApply/{applyId}')
-  .onCreate((snap) => {
-    const { groupId, uid } = snap.data()
+  .onCreate(async (groupApplySnapshot) => {
+    const { groupId, uid } = groupApplySnapshot.data()
 
     const groupRef = db.collection('groups').doc(groupId)
+    const payingGroup = await isPayingGroup(groupRef)
 
-    return db.runTransaction(async (t) => {
-      const group = (await t.get(groupRef)).data()
-      const payingGroup = group.price > 0
+    const membersKey = payingGroup ? 'awaitingMembers' : 'members'
 
-      const membersKey = payingGroup ? 'awaitingMembers' : 'members'
-
-      t.update(groupRef, {
-        [membersKey]: admin.firestore.FieldValue.arrayUnion(uid),
-      })
-      t.update(snap.ref, {
-        status: payingGroup ? 'applied' : 'validated',
-        appliedAt: admin.firestore.FieldValue.serverTimestamp(),
-        validatedAt: payingGroup
-          ? null
-          : admin.firestore.FieldValue.serverTimestamp(),
-      })
+    const batch = db.batch()
+    batch.update(groupRef, {
+      [membersKey]: admin.firestore.FieldValue.arrayUnion(uid),
     })
+    batch.update(groupApplySnapshot.ref, {
+      status: payingGroup ? 'applied' : 'validated',
+      appliedAt: admin.firestore.FieldValue.serverTimestamp(),
+      validatedAt: payingGroup
+        ? null
+        : admin.firestore.FieldValue.serverTimestamp(),
+    })
+
+    return batch.commit()
   })
