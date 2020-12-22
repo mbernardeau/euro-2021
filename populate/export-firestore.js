0857cf17ebf1a51da3603c8320882fb7e88c4f73
Ivanlb <ivan.le-bihan@hupi.fr>
2020-12-22 22:56:38 +0100

    Chgt srviceAccount et initializeApp

--- populate/export-firestore.js
+++ populate/export-firestore.js
@@ -1,11 +1,10 @@
 const admin = require('firebase-admin')
 const fs = require('fs')
-const serviceAccount = require('./euro2021-3d006-firebase-adminsdk-swizj-1e6bc3a6fe.json')
+const serviceAccount = require('./euro2021-3d006-firebase-adminsdk-swizj.json')
 const { firestoreImport } = require('node-firestore-import-export')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://road-to-russia-540bf.firebaseio.com',
 })
 
 const db = admin.firestore()
--- populate/import-firestore.js
+++ populate/import-firestore.js
@@ -1,25 +1,29 @@
 const admin = require('firebase-admin')
 const fs = require('fs')
-const serviceAccount = require('./pronostics-47048-firebase-adminsdk-5jyex-2c08d51542')
+const serviceAccount = require('./euro2021-3d006-firebase-adminsdk-swizj.json')
 const { firestoreExport } = require('node-firestore-import-export')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://pronostics-47048.firebaseio.com',
 })
 
 const db = admin.firestore()
 
-const exportCollection = collectionName => {
-  firestoreExport(db.collection(collectionName)).then(data => {
+const exportCollection = (collectionName) => {
+  firestoreExport(db.collection(collectionName)).then((data) => {
     const jsonData = JSON.stringify(data, null, 2)
-    fs.writeFile(`./firestore-data/${collectionName}.json`, jsonData, 'utf8', err => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      console.log(`Export for ${collectionName} complete`)
-    })
+    fs.writeFile(
+      `./firestore-data/${collectionName}.json`,
+      jsonData,
+      'utf8',
+      (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        console.log(`Export for ${collectionName} complete`)
+      },
+    )
   })
 }
 
