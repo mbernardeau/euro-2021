99637129978b2ad8443828f52fd0e3e056f79b8c
naustra <lebihanp@hotmail.fr>
2021-02-27 02:53:13 +0100

    Correct warning for linting prod

--- src/screens/Matches/Match/Odds/Odds.jsx
+++ src/screens/Matches/Match/Odds/Odds.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React /*, { Fragment }*/ from 'react'
 import PropTypes from 'prop-types'
 import Tooltip from '@material-ui/core/Tooltip'
 import padStart from 'lodash/padStart'
