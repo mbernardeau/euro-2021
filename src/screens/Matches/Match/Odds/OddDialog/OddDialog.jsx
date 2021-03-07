a2d6ed356f0cbbcf817dd43f4a3beb3cb8b5d4aa
naustra <lebihanp@hotmail.fr>
2021-03-07 21:46:26 +0100

    Fix

--- src/screens/Matches/Match/Odds/OddDialog/OddDialog.jsx
+++ src/screens/Matches/Match/Odds/OddDialog/OddDialog.jsx
@@ -5,7 +5,7 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 import {
   Button,
   DialogActions,
