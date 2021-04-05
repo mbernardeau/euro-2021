4a18659731ed60d041ceea4fdae89b4f38dc9e32
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2021-04-05 15:11:35 +0200

    Add ldc instance

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,9 @@
 /* eslint-disable no-restricted-globals */
 /* eslint-disable no-undef */
 /* eslint-env worker */
-importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js')
-
-firebase.initializeApp({
-  apiKey: 'AIzaSyDhYtLgdmUJS08BUl0bN3LHxND5a-KId7g',
-  authDomain: 'euro2021-3d006.firebaseapp.com',
-  databaseURL: 'https://euro2021-3d006.firebaseio.com',
-  projectId: 'euro2021-3d006',
-  storageBucket: 'euro2021-3d006.appspot.com',
-  messagingSenderId: '227571739008',
-  appId: '1:227571739008:web:6024da3fda61ac0cb13965',
-  measurementId: 'G-VHC3B020NE',
-})
+importScripts('/__/firebase/8.3.0/firebase-app.js')
+importScripts('/__/firebase/8.3.0/firebase-messaging.js')
+importScripts('/__/firebase/init.js')
 
 const messaging = firebase.messaging()
 
