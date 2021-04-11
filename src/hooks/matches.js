488f836f5fcc62c93f69615cda4b4c3ee1150996
naustra <lebihanp@hotmail.fr>
2021-04-11 23:08:16 +0200

    Order matches by date

--- src/hooks/matches.js
+++ src/hooks/matches.js
@@ -1,7 +1,7 @@
 import { useFirestore, useFirestoreCollection } from 'reactfire'
 
 export const useMatches = () => {
-  const query = useFirestore().collection('matches')
+  const query = useFirestore().collection('matches').orderBy('dateTime')
 
   return useFirestoreCollection(query).data?.docs
 }
