547e35d22213e3ac757d7e8b961b4dc52bf2e45b
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2021-04-17 09:25:41 +0200

    Fix async displayBets

--- populate/displayBets.js
+++ populate/displayBets.js
@@ -10,37 +10,29 @@ admin.initializeApp({
 
 const db = admin.firestore()
 
-db.collection('bets')
-  .get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      const { uid, matchId, pointsWon } = doc.data()
-      let nameTeamA, nameTeamB, userName
+const displayBets = async () => {
+  const querySnapshot = await db.collection('bets').get()
 
-      db.collection('matches')
-        .doc(matchId)
-        .get()
-        .then((matchDoc) => {
-          const { teamA, teamB } = matchDoc.data()
-          db.collection('teams')
-            .doc(teamA)
-            .get()
-            .then((teamDoc) => {
-              nameTeamA = teamDoc.data().name
-            })
-          db.collection('teams')
-            .doc(teamB)
-            .get()
-            .then((teamDoc) => {
-              nameTeamB = teamDoc.data().name
-            })
-          db.collection('opponents')
-            .doc(uid)
-            .get()
-            .then((userDoc) => {
-              userName = userDoc.data().displayName
-            })
-        })
+  return Promise.all(
+    querySnapshot.docs.map(async (doc) => {
+      const { uid, matchId, pointsWon } = doc.data()
+
+      const { teamA, teamB } = (
+        await db.collection('matches').doc(matchId).get()
+      ).data()
+      const { name: nameTeamA } = (
+        await db.collection('teams').doc(teamA).get()
+      ).data()
+      const { name: nameTeamB } = (
+        await db.collection('teams').doc(teamB).get()
+      ).data()
+      const { displayName: userName } = (
+        await db.collection('opponents').doc(uid).get()
+      ).data()
+
       console.log(userName.padEnd(20), nameTeamA, nameTeamB, pointsWon)
-    })
-  })
+    }),
+  )
+}
+
+displayBets().then(() => process.exit(0))
