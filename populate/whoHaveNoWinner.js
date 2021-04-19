9115a96b12d68cce5eaa3e687bae15df7fe26b7b
naustra <lebihanp@hotmail.fr>
2021-04-20 00:40:12 +0200

    add No Winner found request

---
+++ populate/whoHaveNoWinner.js
@@ -0,0 +1,28 @@
+/*
+ * Display which user has no winnerTeam
+ */
+const { serviceAccount } = require('./chooseDatabase.js')
+
+const admin = require('firebase-admin')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+const db = admin.firestore()
+
+const displayForgottenBets = async () => {
+  const querySnapshot = await db
+    .collection('opponents')
+    .where('winnerTeam', '==', null)
+    .get()
+
+  const result = await Promise.all(
+    querySnapshot.docs.map(async (doc) => {
+      const { displayName, uid } = doc.data()
+      return { displayName, uid }
+    }),
+  )
+  console.table(result)
+}
+
+displayForgottenBets().then(() => process.exit(0))
