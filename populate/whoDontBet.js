423417ce617b5776f4b9c773ad493cbfd2575393
Mathias Bernardeau <mathias.bernardeau@zenika.com>
2021-04-19 20:23:42 +0200

    Fix whoDontBetScript

--- populate/whoDontBet.js
+++ populate/whoDontBet.js
@@ -12,10 +12,10 @@ const db = admin.firestore()
 
 const displayForgottenBets = async () => {
   const querySnapshot = await db.collection('matches').get()
+  const querySnapshotUser = await db.collection('opponents').get()
 
   const table = await Promise.all(
     querySnapshot.docs.map(async (doc) => {
-      const querySnapshotUser = await db.collection('opponents').get()
       const { teamA, teamB } = doc.data()
       const matchId = doc.id
 
@@ -28,16 +28,10 @@ const displayForgottenBets = async () => {
       ).data()
 
       return await Promise.all(
-        querySnapshotUser.docs.map(async (doc) => {
-          const { displayName, uid } = doc.data()
+        querySnapshotUser.docs.map(async (user) => {
+          const { displayName, uid } = user.data()
 
-          const exist = true
-
-          // await db
-          //   .collection('bets')
-          //   .where('matchId', '==', matchId)
-          //   .where('uid', '==', uid)
-          //   .get()
+          const exist = await betExists(uid, matchId)
 
           return {
             exist,
@@ -48,16 +42,34 @@ const displayForgottenBets = async () => {
           }
         }),
       )
-
-      // if (!exist) {
-      // return {
-      //   betTable,
-      // }
-      // }
     }),
   )
 
-  console.table(table)
+  console.table(flatten(table))
+}
+
+/**
+ * Prédicat *asynchrone* pour vérifier si un pari existe.
+ * Note: Ici on ne vérifie que l'existance du document, pas sa validité.
+ *
+ * @param uid Identifiant de l'utilisateur
+ * @param matchId Identifiant du match
+ * @returns `true` si le document existe, `false` sinon
+ */
+async function betExists(uid, matchId) {
+  const betSnapshot = await db.collection('bets').doc(`${matchId}_${uid}`).get()
+
+  return betSnapshot.exists
+}
+
+/**
+ * Flattens array a single level deep.
+ * @param {Array} array Array of arrays
+ *
+ * @returns Flattened array
+ */
+function flatten(array) {
+  return array.reduce((a, b) => a.concat(b), [])
 }
 
 displayForgottenBets().then(() => process.exit(0))
