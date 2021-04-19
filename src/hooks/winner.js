5fb70f6e1ad66312f1d5c88d8e9ef3dfe92fe518
naustra <lebihanp@hotmail.fr>
2021-04-19 21:50:32 +0200

    Revert "Transfer winnerTeam to opponents"
    This reverts commit cd82fdb710787a6da1930d51ed254976eb3b8132.

--- src/hooks/winner.js
+++ src/hooks/winner.js
@@ -1,17 +1,17 @@
 import { useCallback } from 'react'
-import { useWinnerTeam } from './opponent'
+import { useUserProfile } from './user'
 import { useFirestore } from 'reactfire'
 import { useSnackbar } from 'notistack'
 
 export const useSelectedWinner = () => {
-  const { uid, winnerTeam } = useWinnerTeam()
+  const { uid, winnerTeam } = useUserProfile()
   const firestore = useFirestore()
   const { enqueueSnackbar } = useSnackbar()
 
   const updater = useCallback(
     (team) =>
       firestore
-        .collection('opponents')
+        .collection('users')
         .doc(uid)
         .update({
           [`winnerTeam`]: team,
