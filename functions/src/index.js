9c0fd8e5bdf4201edfd4ae19d571b8e807022ab2
naustra <lebihanp@hotmail.fr>
2021-05-19 17:40:06 +0200

    Schedule saves

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,11 +2,13 @@ const admin = require('firebase-admin')
 admin.initializeApp()
 
 const { updateScore } = require('./updateScore')
+const { saveData } = require('./saveData')
 const users = require('./users')
 const groups = require('./groups')
 const notifications = require('./notifications')
 
 exports.updateScore = updateScore
+exports.saveData = saveData
 exports.users = users
 exports.groups = groups
 exports.notifications = notifications
