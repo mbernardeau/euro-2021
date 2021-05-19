f8d13560c717cd9d91157e33fc1aa9c3aae21af1
naustra <lebihanp@hotmail.fr>
2021-05-19 16:23:02 +0200

    Use a launch Bet date to accept bet or not (#207)

--- src/screens/HomePage/FinalWinner/FinalWinner.jsx
+++ src/screens/HomePage/FinalWinner/FinalWinner.jsx
@@ -2,7 +2,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
 import React, { Suspense } from 'react'
-import { useSelectedWinner, useCompetitionStartDate } from '../../../hooks'
+import { useSelectedWinner, useCompetitionData } from '../../../hooks'
 import './FinalWinner.scss'
 import FinalWinnerChoice from './FinalWinnerChoice'
 import isPast from 'date-fns/isPast'
@@ -11,7 +11,7 @@ const FinalWinner = () => {
   const [team, saveWinner] = useSelectedWinner()
 
   const CompetitionStartDate = new Date(
-    useCompetitionStartDate().startDate.seconds * 1000,
+    useCompetitionData().startDate.seconds * 1000,
   )
 
   const handleChange = (e) => {
--- src/screens/Matches/Matches.jsx
+++ src/screens/Matches/Matches.jsx
@@ -3,9 +3,11 @@ import Tab from '@material-ui/core/Tab'
 import Tabs from '@material-ui/core/Tabs'
 import map from 'lodash/map'
 import React, { Suspense, useEffect, useMemo, useState } from 'react'
-import { useMatches } from '../../hooks'
+import { useCompetitionData, useMatches } from '../../hooks'
 import Match from './Match'
+import { Typography } from '@material-ui/core'
 import './matches.scss'
+import { isPast } from 'date-fns'
 
 const Matches = () => {
   const [selectedTab, setSelectedTab] = useState(0)
@@ -40,7 +42,9 @@ const Matches = () => {
     [comparingDate, matches, selectedTab],
   )
 
-  return (
+  const LaunchBetDate = new Date(useCompetitionData().launchBet.seconds * 1000)
+
+  return isPast(LaunchBetDate) ? (
     <>
       <AppBar position="fixed" className="matches-tab-bar">
         <Tabs value={selectedTab} onChange={handleTabChange} centered>
@@ -54,6 +58,12 @@ const Matches = () => {
         ))}
       </div>
     </>
+  ) : (
+    <Typography variant="h1">
+      ⚠ Les pronostics seront accessibles à partir du 5 Juin ! D'ici là, vous
+      pouvez créer votre groupe et vous inscrire aux notifications pour être
+      prévenu de toutes les actualité du site !
+    </Typography>
   )
 }
 
