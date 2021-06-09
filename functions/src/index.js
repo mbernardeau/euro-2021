da4e90aa57c44b161277b6085d6b3ba8d9253ed3
naustra <lebihanp@hotmail.fr>
2021-06-09 12:04:37 +0200

    Schedule save (#211)
    * Schedule saves
    
    * Fix
    
    * Prise en compte relecture
    
    * Fix and document saveData
    
    Co-authored-by: Mathias Bernardeau <mathias.bernardeau@outlook.com>

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,8 +5,10 @@ const { updateScore } = require('./updateScore')
 const users = require('./users')
 const groups = require('./groups')
 const notifications = require('./notifications')
+const chore = require('./chore')
 
 exports.updateScore = updateScore
 exports.users = users
 exports.groups = groups
 exports.notifications = notifications
+exports.chore = chore
