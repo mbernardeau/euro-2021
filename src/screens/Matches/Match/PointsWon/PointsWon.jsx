1c58430e795d946a2ca4c65cc43ead8edcc4b774
naustra <lebihanp@hotmail.fr>
2021-06-25 00:06:36 +0200

    Disable tooltip calculus points

--- src/screens/Matches/Match/PointsWon/PointsWon.jsx
+++ src/screens/Matches/Match/PointsWon/PointsWon.jsx
@@ -2,52 +2,54 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Typography from '@material-ui/core/Typography'
-import Tooltip from '@material-ui/core/Tooltip'
+// import Tooltip from '@material-ui/core/Tooltip'
 
 import './PointsWon.scss'
 
 // Proxi points
-const proxiCoeff = [1, 0.6, 0.35, 0.2, 0.1]
-const proxiText = [
-  `🤩 100% de `,
-  `😐 60% de `,
-  `😐 35% de `,
-  `😐 20% de `,
-  `😐 10% de `,
-]
+// const proxiCoeff = [1, 0.6, 0.35, 0.2, 0.1]
+// const proxiText = [
+//   `🤩 100% de `,
+//   `😐 60% de `,
+//   `😐 35% de `,
+//   `😐 20% de `,
+//   `😐 10% de `,
+// ]
 
-const round = (value, decimals) =>
-  Number(`${Math.round(`${value}e${decimals}`)}${`e-${decimals}`}`)
+// const round = (value, decimals) =>
+//   Number(`${Math.round(`${value}e${decimals}`)}${`e-${decimals}`}`)
 
-const getCalculus = (odd, proxiLevel) => {
-  const calculText = `${odd} = ${round(proxiCoeff[proxiLevel] * odd, 2)}`
-  return proxiLevel ? proxiText[proxiLevel] + calculText : '0 + 0 = 😶'
-}
+// const getCalculus = (odd, proxiLevel) => {
+//   const calculText = `${odd} = ${round(proxiCoeff[proxiLevel] * odd, 2)}`
+//   return proxiLevel ? proxiText[proxiLevel] + calculText : '0 + 0 = 😶'
+// }
 
 const PointsWon = ({ pointsWon, proxi, scores, odds }) => {
   if (!scores) return null
 
   // Check proxi
-  const { A, B } = scores
-  const nbButs = A + B
-  const oddScore = nbButs < 7 ? odds[`P${A}${B}`] : odds.Pautre
+  // const { A, B } = scores
+  // const nbButs = A + B
+  // const oddScore = nbButs < 7 ? odds[`P${A}${B}`] : odds.Pautre
 
   return (
     <div className="points-won-container">
       {
-        <Tooltip
-          title={getCalculus(oddScore, proxi)}
-          placement="right"
-          enterTouchDelay={0}
-        >
-          <Typography variant="h3" style={{ fontWeight: 'bold' }}>
-            {pointsWon || 0} pts
-          </Typography>
-        </Tooltip>
+        <Typography variant="h3" style={{ fontWeight: 'bold' }}>
+          {pointsWon || 0} pts
+        </Typography>
       }
     </div>
   )
 }
+
+//  <Tooltip
+//           title={getCalculus(oddScore, proxi)}
+//           placement="right"
+//           enterTouchDelay={0}
+//         >
+
+//         </Tooltip>
 
 PointsWon.propTypes = {
   pointsWon: PropTypes.number,
